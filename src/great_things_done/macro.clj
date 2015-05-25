43a21daf50e6fcbb7aedb6173bcc0d86b776ead9
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2015-05-25 02:06:59 +0200

    Better version of `for-os` macro

--- src/great_things_done/macro.clj
+++ src/great_things_done/macro.clj
@@ -1,14 +1,21 @@
-(ns great_things_done.macro)
+(ns great-things-done.macro)
 
 (defmacro for-os
   "Execute code depending on the current OS"
   [& body]
-  (let [os-map# (into {}
-                       (into []
-                             (map #(into [] %)
-                                  (partition 2 body))))]
-     (if (even? (count body))
-       (if-let [fun# (get os-map# (System/getProperty "os.name"))]
-         fun#
-         (get os-map# :default))
-       (throw (Exception. "Wrong number of args. `for-os` should have an even number of arguments")))))
+  (let [platform-translation# {"darwin"  "Mac OS X"
+                               "win32"   "Windows"
+                               "freebsd" "BSD"
+                               "linux"   "Linux"
+                               "sunos"   "SunOS"}
+        os-map#               (into {}
+                                    (into []
+                                          (map #(into [] %)
+                                               (partition 2 body))))]
+    (if (even? (count body))
+      `(if (contains? ~os-map# (get ~platform-translation#
+                                    js/process.platform))
+                      (get ~os-map# (get ~platform-translation#
+                                         js/process.platform))
+                      ~(get os-map# :default))
+         (throw (Exception. "Wrong number of args. `for-os` should have an even number of arguments")))))
