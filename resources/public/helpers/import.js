f6c2ea07d7d7de4096ad9ba57b3cec302e831a12
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2015-05-27 03:07:52 +0200

    Minor fix

--- resources/public/helpers/import.js
+++ resources/public/helpers/import.js
@@ -1,6 +1,6 @@
 console.log("Dragons");
 window.importScript = function(string) {
-  var projects = great_things_done.db.projects();
+  var projects = great_things_done.db.list_of_projects() || [];
   var fn = eval("(" + string + ")");
   var result = fn(projects) || [];
 
