7d88a9bac66ae7d2a709ebc9820f5b0fa8a08e42
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2015-10-12 20:11:15 +0200

    Ignore generated files

--- resources/js/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "great-things-done", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
--- resources/js/out/figwheel/connect.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.7.122 {}
-goog.provide('figwheel.connect');
-goog.require('cljs.core');
-goog.require('figwheel.client');
-goog.require('figwheel.client.utils');
-figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"build-id","build-id",1642831089),"great-things-done",new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3449/figwheel-ws"], null));
-
-//# sourceMappingURL=connect.js.map
