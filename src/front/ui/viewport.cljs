d9c3c3404e4c67174a667c99b6f88f2f90946774
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2015-10-20 21:38:00 +0200

    First draft of task list

--- src/front/ui/viewport.cljs
+++ src/front/ui/viewport.cljs
@@ -1,6 +1,7 @@
 (ns ui.viewport
   (:use [jayq.core :only [$]])
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [gtd.state :as state]
+            [reagent.core :as reagent :refer [atom]]))
 
 (def ^{:private true
        :no-docs true} shell (js/require "shell"))
@@ -38,21 +39,63 @@
               task
               task-id]))]])
 
+(defn render-done
+  [task task-id]
+  [:li.todo
+   {:class (build-to-do-class task
+                              task-id)}
+   [:div.input.check-box
+    {:on-click (fn []
+                 (state/update-task! task
+                                     :done false))}]
+   [:p (:name task)]])
+
 (defn- render-no-to-do
   []
   [:div.no-todo-container
    "NO to do"])
 
-(defn render-tasks-for
-  [tasks]
-  (let [tasks-done  (filter #(:done %)
-                            tasks)
+(defn- render-dones
+  [tasks task-id]
+  [:div.done-container
+   [:ul.doness
+    (doall (for [task tasks]
+             ^{:key (:id task)}
+             [render-done
+              task
+              task-id]))]])
+
+(defn- render-no-done
+  []
+  [:div.no-done-container
+   "NO done"])
+
+(defn plain-render-tasks-for
+  [tasks task-id]
+  (let [ts          (map #(state/get-task-by-id (:id %))
+                         tasks)
+        tasks-done  (filter #(:done %)
+                            ts)
         tasks-to-do (remove #(:done %)
-                            tasks)]
-    [:div.tasks
-     (if (empty? tasks-to-do)
-       [render-no-to-do]
-       [render-to-dos tasks-to-do])]))
+                            ts)]
+    [:div#tasks-container
+     [:div
+      "To Do"
+      (if (empty? tasks-to-do)
+        [render-no-to-do]
+        [render-to-dos tasks-to-do task-id])]
+     [:div
+      "Done"
+      (if (empty? tasks-done)
+        [render-no-done]
+        [render-dones tasks-done task-id])]]))
+
+(def ^:private render-tasks-for
+  (with-meta plain-render-tasks-for
+    {:component-did-mount
+     (fn []
+       (.perfectScrollbar ($ :#tasks-container)
+                          (clj->js {:suppressScrollX true})))}))
 
 (defmulti viewport-container-component (fn [id _] id))
 
